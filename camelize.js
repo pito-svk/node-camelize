238948e537a32f2d58b4d03f905edbbfc480474f
Peter Parada <parad.peter@gmail.com>
2017-06-15 20:10:09 +0200

    Small typo

--- camelize.js
+++ camelize.js
@@ -2,7 +2,7 @@ const camelCase = require('lodash.camelcase')
 const mapKeys = require('lodash.mapkeys')
 
 function camelize (o) {
-  return mapkeys(o, (_, k) => camelCase(k))
+  return mapKeys(o, (_, k) => camelCase(k))
 }
 
 module.exports = camelize
