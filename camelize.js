de1ae8cdff592a75aa8d4457f08bca3e08d634c8
Peter Parada <parad.peter@gmail.com>
2017-06-16 10:46:14 +0200

    Allow using plain string to camelize

--- camelize.js
+++ camelize.js
@@ -2,7 +2,11 @@ const camelCase = require('lodash.camelcase')
 const mapKeys = require('lodash.mapkeys')
 
 function camelize (o) {
-  return mapKeys(o, (_, k) => camelCase(k))
+  if (typeof o === 'string') {
+    return camelCase(o)
+  } else {
+    return mapKeys(o, (_, k) => camelCase(k))
+  }
 }
 
 module.exports = camelize
